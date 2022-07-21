62d667e1b62a63d5ed1f44d57460fd825358809a
Mashimo <toshichika-mashimo@retty.me>
2022-07-21 17:06:56 +0900

    fix sql

--- sql/migrate.sh
+++ sql/migrate.sh
@@ -7,10 +7,10 @@ fi
 
 for file in `\find . -maxdepth 1 -type f | sort`; do
     if [ $file == './seed.sql' ]; then
-        cat seed.sql
-#        echo $file
+#        cat seed.sql
+        echo $file
     elif [ `echo $file | grep '\.sql'` ]; then
         echo $file
-#        mysqldef seeft -u mysql -p pwd --skip-drop < $file
+        mysqldef seeft -u mysql -p pwd --skip-drop < $file
     fi
 done
