ce75c56c7e9f7d8537bb52756084a53aec924043
Mashimo <mashita1023@gmail.com>
2022-07-28 18:45:19 +0900

    getShiftsByUserのsql完成

--- lib/interface/repository/shift_repository.dart
+++ lib/interface/repository/shift_repository.dart
@@ -30,9 +30,25 @@ SELECT
   tasks.updated_at as task_updated_at,
   tasks.deleted_at as task_deleted_at,
   shifts.year_id,
+  years.year as year_name,
+  years.created_at as year_created_at,
+  years.updated_at as year_updated_at,
+  years.deleted_at as year_deleted_at,
   shifts.date_id,
+  dates.date as date_name,
+  dates.created_at as date_created_at,
+  dates.updated_at as date_updated_at,
+  dates.deleted_at as date_deleted_at,
   shifts.time_id,
+  times.time as time_name,
+  times.created_at as time_created_at,
+  times.updated_at as time_updated_at,
+  times.deleted_at as time_deleted_at,
   shifts.weather_id,
+  weathers.weather as weather_name,
+  weathers.created_at as weather_created_at,
+  weathers.updated_at as weather_updated_at,
+  weathers.deleted_at as weather_deleted_at,
   shifts.is_attendance,
   shifts.created_user_id,
   shifts.updated_user_id,
@@ -45,7 +61,8 @@ LEFT JOIN tasks ON shifts.task_id = tasks.id
 LEFT JOIN years ON shifts.year_id = years.id
 LEFT JOIN dates ON shifts.date_id = dates.id
 LEFT JOIN times ON shifts.time_id = times.id
-LEFT JOIN weathers ON shifts.weather_id = weathers.id;
+LEFT JOIN weathers ON shifts.weather_id = weathers.id
+WHERE user_id=${req.id};
 ''';
 
     List<Map<String, dynamic>> data = await database.finds(ctx, sql);
@@ -77,22 +94,38 @@ LEFT JOIN weathers ON shifts.weather_id = weathers.id;
         ),
         year: Year(
           id: d['year_id'],
+          year: d['year_name'],
+          createdAt: d['year_created_at'],
+          updatedAt: d['year_updated_at'],
+          deletedAt: d['year_deleted_at'],
         ),
         date: Date(
           id: d['date_id'],
+          date: d['date_name'],
+          createdAt: d['date_created_at'],
+          updatedAt: d['date_updated_at'],
+          deletedAt: d['date_deleted_at'],
         ),
         time: Time(
           id: d['time_id'],
+          time: d['time_name'],
+          createdAt: d['date_created_at'],
+          updatedAt: d['date_updated_at'],
+          deletedAt: d['date_deleted_at'],
         ),
         weather: Weather(
           id: d['weather_id'],
+          weather: d['weather_name'],
+          createdAt: d['weather_created_at'],
+          updatedAt: d['weather_updated_at'],
+          deletedAt: d['weather_deleted_at'],
         ),
         isAttendance: d['is_attendance'] == 1,
         createdUserId: d['created_user_id'],
         updatedUserId: d['updated_user_id'],
-        createdAt: d['created_at'],
-        updatedAt: d['updated_at'],
-        deletedAt: d['deleted_at'],
+        createdAt: d['shift_created_at'],
+        updatedAt: d['shift_updated_at'],
+        deletedAt: d['shift_deleted_at'],
       );
 
       if (shift.isDeleted) {
