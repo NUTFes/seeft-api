b6dc90a0987a95d8e55db164eb4fca39fe04b523
Sato <satoma.2001.0920@gmail.com>
2022-09-04 20:14:01 +0900

    testのプログラム以外のエラー修正

--- lib/usecase/repository/shift_repository.dart
+++ lib/usecase/repository/shift_repository.dart
@@ -3,6 +3,6 @@ import '../../entity/entity.dart';
 abstract class ShiftRepository {
   Future<List<Shift>> getShiftsByUser(ctx, User req);
   Future<List<Shift>> getShiftsByUserAndDateAndWeather(ctx, Shift req);
-  Future<List<Shift>> getShiftsByYear(ctx, User req);
+  Future<List<Shift>> getShiftsByYear(ctx, Shift req);
   Future<List<Shift>> getShiftsByDate(ctx, Shift req);
 }
